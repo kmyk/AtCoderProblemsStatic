96baa45b9d5008c7e9f43e4102d30450340315e4
kenkoooo <kenkou.n@gmail.com>
2019-06-18 00:31:26 +0900

    import the frontend from the original AtCoderProblems

---
+++ precache-manifest.e1eacb77a069482249f1b55b0131b07e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8c4ce1c6b995077a0df",
+    "url": "/AtCoderProblemsStatic/static/js/runtime~main.d8c4ce1c.js"
+  },
+  {
+    "revision": "f006cb81c6ed36806aca",
+    "url": "/AtCoderProblemsStatic/static/js/main.f006cb81.chunk.js"
+  },
+  {
+    "revision": "2705599800946e800aa6",
+    "url": "/AtCoderProblemsStatic/static/js/2.27055998.chunk.js"
+  },
+  {
+    "revision": "f006cb81c6ed36806aca",
+    "url": "/AtCoderProblemsStatic/static/css/main.f111a761.chunk.css"
+  },
+  {
+    "revision": "2705599800946e800aa6",
+    "url": "/AtCoderProblemsStatic/static/css/2.0707c12e.chunk.css"
+  },
+  {
+    "revision": "525994248ee08f50bcaca7289f4e18c7",
+    "url": "/AtCoderProblemsStatic/index.html"
+  }
+];
